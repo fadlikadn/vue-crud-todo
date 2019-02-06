66f33f033788cd56c067785380288d057d3d3b02
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-02-06 14:55:36 +0700

    Todo App - Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+import VueLogger from 'vuejs-logger';
+
+const options = {
+  isEnabled: true,
+  logLevel: 'debug',
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: false,
+  separator: '|',
+  showConsoleColors: true
+};
+
+Vue.use(VueLogger, options);
+
+// eslint-disabled no-new 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  template: '<App/>',
+  components: {App}
+});
